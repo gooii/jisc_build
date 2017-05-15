f712205d3c346cdbae9143d09064a858807c4371
Benji Soar <benji@gooii.com>
2017-05-15 16:17:38 +0100

    Corrections to login.php script.

--- server/login.php
+++ server/login.php
@@ -1,14 +1,14 @@
 <html>
 <head>
   <?php
-  echo "<meta http-equiv=\"refresh\" content=\"0;url=/Shibboleth.sso/Login?target=https://".$_SERVER{HTTP_HOST}.$_SERVER{REQUEST_URI}."\">";
+  echo "<meta http-equiv=\"refresh\" content=\"0;url=/Shibboleth.sso/Login?target=https://".$_SERVER{'HTTP_HOST'}.$_SERVER{'REQUEST_URI'}."\">";
   ?>
 </head>
 <body>
 <!-- <h1>You are being redirected to login via Shibboleth</h1>
 <p>Please click
 <?php
-echo " <a href=\"/Shibboleth.sso/Login?target=https://".$_SERVER{HTTP_HOST}.$_SERVER{REQUEST_URI}."\">here</a> "
+echo " <a href=\"/Shibboleth.sso/Login?target=https://".$_SERVER{'HTTP_HOST'}.$_SERVER{'REQUEST_URI'}."\">here</a> "
 ?>
 to continue.</p> -->
 </body>
